da08004a50f58d2a8dbcd174c62757c3ee14fc07
Zeca Romero <zeca.85@gmail.com>
2024-04-15 15:30:56 -0300

    IF E ELSE

---
+++ condicao.js
@@ -0,0 +1,9 @@
+var vel  = 61
+
+console.log(`A velocidade do seu carro é ${vel} KM/h`)
+
+if ( vel > 60) { //Condição Simples
+    console.log(`Voce Ultrapassou o limite de velocidade. Multado! `)
+
+}
+console.log ('Dirija sempre usando o cinto de segurança')
